7d980fbf1bbed66b474e3d344263f6f2e7e1f3f5
javitoba12 <javitoba12@gmail.com>
2026-02-07 12:37:28 +0100

    cambios laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,13 +18,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+   /* public function boot(): void
     {
         //
         if (DB::getDriverName() === 'sqlite') {
             DB::statement('PRAGMA foreign_keys=ON;');
         }//Comentar todo si no funciona
-    }
+    }*/
 
 
 }

0db7035b7f26fdc07843b58e423155fb1f33eb83
javitoba12 <javitoba12@gmail.com>
2026-02-11 12:40:07 +0100

    cambios proyecto

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         
-        //
+        $middleware->trustProxies(at: '*');
         $middleware->web(append:[
             App\Http\Middleware\ChequeoRol::class,
            // App\Http\Middleware\ChequeoTema::class

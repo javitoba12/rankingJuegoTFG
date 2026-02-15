f08c22d44102b3c0dd39e5af9157f3173d867bfa
javitoba12 <javitoba12@gmail.com>
2026-02-15 18:16:54 +0100

    cambios en steam news api config

--- config/steam_news_api_config.php
+++ config/steam_news_api_config.php
@@ -1,10 +1,23 @@
 <?php
 
-return [
+/*return [
 
     'appid' => 582010, //id del juego
     'count' => 5, //numero de noticias que quiero extraer
     'maxlength' => 300 // numero de caracteres por noticias, dentro de su contenido
+
+    //appid de stalker 2 --> 1643320
+
+];*/
+
+return [
+
+    'appid' => env('STEAM_APP_ID', 582010), //id del juego, con env miro en el archivo env, o en las variables de entorno de railway(si estoy en produccion)
+    //si existe la variable STEAM_APP_ID, y en caso de no existir, uso el valor por defecto para esa variable (582010, la id de monster hunter en steam), lo 
+    //mismo aplica al resto de variables que devuelvp en este .config
+    
+    'count' => env('STEAM_COUNT', 5), //numero de noticias que quiero extraer
+    'maxlength' => env('STEAM_MAX_LENGTH', 300) // numero de caracteres por noticias, dentro de su contenido
 
     //appid de stalker 2 --> 1643320
 
